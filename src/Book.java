237bb7945ec376ac13ded4d7fb3f92364206d0a6
AntonGorbuno <joongleboo01@gmail.com>
2022-05-19 23:44:45 +0300

    Доделал

--- src/Book.java
+++ src/Book.java
@@ -1,10 +1,24 @@
 public class Book {
+    private Author author;
+    private Author author2;
     private String nameBook;
     private int bookYear;
 
     public Book(String nameBook, int bookYear) {
+        Author author1 = new Author("Stephen", "King");
+        Author author2 = new Author("Govard","Lovecraft");
+        this.author = author1;
+        this.author2 = author2;
         this.nameBook = nameBook;
         this.bookYear = bookYear;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public Author getAuthor2() {
+        return author2;
     }
 
     public String getNameBook() {
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         Book theDeadZone = new Book("The Dead Zone", 1983);
-        Author stephenKing = new Author("Stephen", "King");
-        System.out.println(theDeadZone.getNameBook() + " " + theDeadZone.getBookYear() + " " + stephenKing.getNameAuthor() + " " + stephenKing.getSurnameAuthor());
+        System.out.println(theDeadZone.getNameBook() + " " + theDeadZone.getBookYear() + " " + theDeadZone.getAuthor().getSurnameAuthor());
         theDeadZone.setBookYear(2014);
         System.out.println("Эту книгу я прочитал в " + theDeadZone.getBookYear());
         Book theWhispererInDarkness = new Book("The Whisperer in Darkness", 1931);
-        Author govardLovecraft = new Author("Govard", "Lovecraft");
-        System.out.println(theWhispererInDarkness.getNameBook() +  " " + theWhispererInDarkness.getBookYear()+ " " + govardLovecraft.getNameAuthor()+ " " + govardLovecraft.getSurnameAuthor());
+        System.out.println(theWhispererInDarkness.getNameBook() +  " " + theWhispererInDarkness.getBookYear()+ " " + theWhispererInDarkness.getAuthor2().getSurnameAuthor());
         theWhispererInDarkness.setBookYear(2020);
         System.out.println("Эту книгу я прочитал в " + theWhispererInDarkness.getBookYear());
     }
